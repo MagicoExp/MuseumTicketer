3b1b3c08be2baf2a26c7bc2c857841a61e08e16e
Vishvaraj18 <vishvaraj.dgkgr22@sinhgad.edu>
2024-11-16 12:23:50 +0530

    added ejs

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path')
+const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
+const app = express();
+// const collection = require('./Schema/user')
+// const post = require('./Schema/post');
+// const { userInfo } = require('os');
+// const { log } = require('console');
+const port = 3001;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'public' )));
+app.set('view engine','ejs');
+app.use(cookieParser());
+
+app.get('/',(req,res)=>{
+    res.render('home');
+});
+app.get('/plan-your-visit',(req,res)=>{
+    res.render('plan_visit');
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
